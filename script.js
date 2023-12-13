8165bf593030e90d44b8fec3fd230e9df5e88a78
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-12-13 14:49:27 +0300

    update

--- script.js
+++ script.js
@@ -1,47 +1,14 @@
-/* CONTACT */
+document.addEventListener("DOMContentLoaded", function () {
+  var header = document.querySelector('.header');
+  
+  window.onscroll = function () {
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
+  };
+});
 
-(function () {
-    $("#send_btn").on("click", function () {
-      if ($("#name").val() != "" && $("#email").val() != "" && $("#typemsg").val() != ""){
 
-        setTimeout(function () {
-        $("#message").addClass("sending").text("Sending");
-        $("#send_btn").addClass("sending");
-      }, 0);
 
-      setTimeout(function () {
-        $("#message").addClass("sent").text("Sent");
-        $("#send_btn").addClass("sent");
-      }, 2600);
-      setTimeout(function () {
-        $(".contactForm").css("display", "none");
-        $(".popup").css("display", "block");
-      }, 4000);
-      }
-      else{
-        alert("Please fill all the blanks.")
-      }
-    });
-  });
-  (function () {
-    $("#message").on("click", function () {
-      if ($("#name").val() != "" && $("#email").val() != "" && $("#typemsg").val() != ""){
-        setTimeout(function () {
-        $("#message").addClass("sending").text("Sending");
-        $("#send_btn").addClass("sending");
-      }, 0);
-
-      setTimeout(function () {
-        $("#message").addClass("sent").text("Sent");
-        $("#send_btn").addClass("sent");
-      }, 2600);
-      setTimeout(function () {
-        $(".contactForm").css("display", "none");
-        $(".popup").css("display", "block");
-      }, 4000);
-      }
-      else{
-        alert("Please fill all the blanks.")
-      }   
-    });
-  });
